4db93b4240777ee8fe91204e2106465ce3fbf206
Gavin Staniforth <gstaniforth@sessiondigital.com>
2014-11-20 11:00:32 +0000

    Optional abstract

--- Something/SomethingAbstract.php
+++ Something/SomethingAbstract.php
@@ -7,8 +7,19 @@ use Stubborn\StubbornAwareInterface;
 abstract class SomethingAbstract implements StubbornAwareInterface
 {
 
+    /**
+     * @inheritdoc
+     */
     public function getRetryNumber()
     {
         return 4;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRetryWaitSeconds()
+    {
+        return 5;
     }
 }
