d5d7854cd2bbe492129a85cd2795bb90d39f4fe1
Gavin Staniforth <gstaniforth@sessiondigital.com>
2014-11-21 11:49:32 +0000

    Updated docblocks based on scrutinizer

--- src/Stubborn/StubbornAwareInterface.php
+++ src/Stubborn/StubbornAwareInterface.php
@@ -19,19 +19,19 @@ interface StubbornAwareInterface
     public function getRetryWaitSeconds();
 
     /**
-     * @return StubbornResponse|2|1|0|null
+     * @return StubbornResponse|self::RETRY_WAIT_ACTION|self::RETRY_ACTION|self::STOP_ACTION|null
      */
     public function run();
 
     /**
      * @param StubbornResponseInterface $response
-     * @return false|2|1|0
+     * @return false|self::RETRY_WAIT_ACTION|self::RETRY_ACTION|self::STOP_ACTION
      */
     public function getHttpActionRequest(StubbornResponseInterface $response);
 
     /**
      * @param \Exception $exception
-     * @return false|2|1|0
+     * @return false|self::RETRY_WAIT_ACTION|self::RETRY_ACTION|self::STOP_ACTION
      */
     public function getExceptionActionRequest(\Exception $exception);
 }
